b80b9f7e2bf1b4892e6977542f4782708eb7307e
Aayyi <julien.lavocat@gmail.com>
2019-05-18 14:30:41 +0200

    Adding base error throwing

--- controllers/app.js
+++ controllers/app.js
@@ -3,5 +3,9 @@
 const HttpError = require("simplified-http-errors").HttpError;
 
 exports.root = async function (req, res) {
+
+    if(req.query.throwError)
+        throw new HttpError("aborted", "User requested an error", {hereAreTheDetails: "The cake is a lie."});
+
     res.send("Hello world!");
 }
